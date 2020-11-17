c128276b1ec7397e58fd3ff4aad5186817e9705a
RamOmri <Omriyram@gmail.com>
2020-11-17 20:04:59 +0100

    created scanner cropper and basic preview screen

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,4 +1,4 @@
-import React, {Image} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,7 +6,9 @@ import {
   View,
   Text,
   StatusBar,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert,
+  Image
 } from 'react-native';
 
 import {
@@ -17,28 +19,46 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import DocumentScanner from "@woonivers/react-native-document-scanner"
+import CustomCrop from "react-native-perspective-image-cropper";
+import ImageSize from 'react-native-image-size'
+import ImgToBase64 from 'react-native-image-base64';
 
 
 export default class Scanner extends React.Component {
 
-
-    state = {
+constructor(props){
+ super(props)
+  this.state = {
       image: null,
-      pdfScannerElement: React.createRef()
+      pdfScannerElement: React.createRef(),
+      isScannerRendered: true,
+      isCropperRendered: false,
+      isImagePreview: false,
+
+      originImg: null,
+      imgURI: null,
+      croppedImage: null,
+      initialImage: null,
+      imageHeight: 0,
+      imageWidth: 0,
+      rectangleCoordinates: null
     }
-  
+   
+}
 
   
 
 
-    render(){
+    renderScanner(){
         return(
          <React.Fragment>
           <DocumentScanner
+          useBase64 = {true}
             ref={this.state.pdfScannerElement}
             style={styles.scanner}
             onPictureTaken={(data) =>{
-              this.props.navigation.navigate('ScanStack', {params:{cropped:data.croppedImage, initial:data.initialImage}, screen: 'imageCrop'})
+              this.handleScannedDocument(data.croppedImage, data.initialImage)
+              //this.props.navigation.navigate('ScanStack', {params:{cropped:data.croppedImage, initial:data.initialImage}, screen: 'imageCrop'})
             }}
             overlayColor="rgba(255,130,0, 0.7)"
             enableTorch={false}
@@ -50,7 +70,7 @@ export default class Scanner extends React.Component {
            
            <View style ={{flex:0.1}}>
                                         <TouchableOpacity
-                                  style={styles.button}
+                                  style={styles.scanButton}
                                   onPress={() =>{
                                          this.state.pdfScannerElement.current.capture()
                                   }}
@@ -64,15 +84,122 @@ export default class Scanner extends React.Component {
           
           </React.Fragment>
         )
+        this.setState({ScanView : this.renderScanner()})
     }
-}
+
+
+
+    async handleScannedDocument(base64Img, init){      
+        await ImageSize.getSize(base64Img).then(size => {
+           this.setState({
+             initialImage: base64Img,
+            originImg: init,
+            imageWidth: size.width,
+            imageHeight: size.height,
+            rectangleCoordinates: {
+              topLeft: { x: -10, y: 10 },
+              topRight: { x: 10, y: 10 },
+              bottomRight: { x: 10, y: -10 },
+              bottomLeft: { x: 10, y: -10 }
+            },
+             isScannerRendered: false,
+            isCropperRendered: true 
+          }) 
+          
+      }).
+      catch(err => alert("Something went wrong while attempting to setup image-croper: " + err));
+
+     
+          
+      }
+
+    updateImage(image, newCoordinates) {
+      this.setState({
+        initialImage: image,
+       // rectangleCoordinates: newCoordinates,
+        isCropperRendered: false,
+        isImagePreview: true,
+      });
+    }
+   
+    crop() {
+      this.customCrop.crop();
+    }
+
+    renderCropper(){
+      return(
+        <View>
+        <View style = {{flex:1, width: this.state.imageWidth/5, height: this.state.imageHeight/5}}>
+          
+       <CustomCrop
+       style = {{
+         marginTop:100,
+         margin: 100, 
+         
+       }}
+    updateImage={this.updateImage.bind(this)}
+    //rectangleCoordinates={this.state.rectangleCoordinates}
+    initialImage={this.state.initialImage}
+    height={this.state.imageHeight}
+    width={this.state.imageWidth}
+    ref={(ref) => this.customCrop = ref}
+    overlayColor="rgba(18,190,210, 1)"
+    overlayStrokeColor="rgba(20,190,210, 1)"
+    handlerColor="rgba(20,150,160, 1)"
+    enablePanStrict={true}
+      /> 
+        
+      </View>
+      <View>
+        <TouchableOpacity  style={styles.cropButton} onPress={this.crop.bind(this)}>
+          <Text>CROP IMAGE</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+      )
+  }
+
+  renderPreview(){
+    return(
+      <View style = {{alignContent: 'center', justifyContent:'center', alignItems:'center'}}>
+      <Image  style={{ marginTop: 40, width: this.state.imageWidth/5, height: this.state.imageHeight/5, borderWidth:5, borderColor: 'black', resizeMode: 'stretch'}}
+      resizeMode='contain' source={{uri: `data:image/gif;base64,${this.state.initialImage}`}} />
+      </View>
+    )
+  }
+
+    render(){
+      return(
+        <React.Fragment>
+             {this.state.isScannerRendered && this.renderScanner()}
+             {this.state.isCropperRendered && this.renderCropper()}
+             {this.state.isImagePreview && this.renderPreview()}
+        </React.Fragment>
+      )
+    }
+  }
+
+  
+
+
 
 const styles = StyleSheet.create({
   scanner: {
     flex: 0.9,
     aspectRatio:undefined
   },
-  button: {
+  cropButton:{
+    top: 600,
+    width: 220,
+    height: 50,
+    backgroundColor: "#711401ff",
+    justifyContent: 'center',
+    alignItems: 'center',
+   marginTop: 20,
+   borderRadius:30,
+   marginBottom: 20,
+  },
+  scanButton: {
     width: 220,
     height: 50,
     backgroundColor: "#711401ff",
--- src/imageCrop.js
+++ src/imageCrop.js
@@ -6,7 +6,8 @@ import {
   View,
   Text,
   StatusBar,
-  Image
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -17,28 +18,76 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import DocumentScanner from "@woonivers/react-native-document-scanner"
+import CustomCrop from "react-native-perspective-image-cropper";
+import ImageSize from 'react-native-image-size'
 
 
 export default class imageCrop extends React.Component {
+constructor(props){
+  super(props)
+  this.state = {
+    croppedImage: this.props.route.params.cropped,
+    initialImage: this.props.route.params.initial,
+    imageHeight: 0,
+    imageWidth: 0,
+    rectangleCoordinates: null
+    
+  }
 
-  state = {
-    imageInit: this.props.route.params.cropped,
-    imageCropped: this.props.route.params.initial
+  ImageSize.getSize(this.props.route.params.initial).then(size => {
+    this.setState({
+      imageWidth: size.width,
+      imageHeight: size.height,
+      rectangleCoordinates: {
+        topLeft: { x: 10, y: 10 },
+        topRight: { x: 10, y: 10 },
+        bottomRight: { x: 10, y: 10 },
+        bottomLeft: { x: 10, y: 10 }
+      }
+    });
+})
+
+}
+
+
+  updateImage(image, newCoordinates) {
+    this.setState({
+      image,
+      rectangleCoordinates: newCoordinates
+    });
+  }
+ 
+  crop() {
+    this.CustomCrop.crop();
   }
     render(){
         return(
-            <ScrollView>
-                <Image
-                   style={{ width: 350, height: 500, marginBottom: 10, borderWidth:5, borderColor: 'black', resizeMode: 'stretch'}}
-                  resizeMode='contain'
-                  source={{uri: this.props.route.params.cropped}}
-                  />  
-                  <Image
-                   style={{ width: 350, height: 500, marginBottom: 10, borderWidth:5, borderColor: 'black', resizeMode: 'stretch'}}
-                  resizeMode='contain'
-                  source={{uri: this.props.route.params.initial}}
-                  />   
-            </ScrollView>
+          <View>
+         <CustomCrop
+          style={{
+            flex: 0,
+            aspectRatio: 1,
+            backgroundColor: 'red',
+            border: '1px solid red',
+          }}
+          updateImage={()=>{
+
+          }}
+          //rectangleCoordinates={this.state.rectangleCoordinates}
+          initialImage=  {require('./me.jpg')}
+          height={this.state.imageHeight}
+          width={this.state.imageWidth}
+          path={this.state.path}
+          ref={ref => (this.CustomCrop = ref)}
+          overlayColor="rgba(18,190,210, 1)"
+          overlayStrokeColor="rgba(20,190,210, 1)"
+          handlerColor="rgba(20,150,160, 1)"
+          enablePanStrict={false}
+        /> 
+          <TouchableOpacity onPress={this.crop.bind(this)}>
+            <Text>CROP IMAGE</Text>
+          </TouchableOpacity>
+        </View>
         )
     }
 }
